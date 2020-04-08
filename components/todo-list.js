a3ce4cb9edc60d76b97b2ae929bb74dba40b6ff2
seabstianjeong <sebastianrcnt@gmail.com>
2020-04-09 00:55:40 +0900

    Modify todo-list.js

--- components/todo-list.js
+++ components/todo-list.js
@@ -3,17 +3,17 @@ import styled from "styled-components";
 
 import TodoItem from "./todo-item";
 
-export default function TodoList() {
-  return (
-    <Wrapper>
-      Todo list가 들어가는 자리입니다!
-      <TodoItem />
-    </Wrapper>
-  );
+export default function TodoList({ todos, completeTodo }) {
+    return (
+        <Wrapper>
+            {todos.sort((todo1, todo2) => {
+                return todo1.dueDate < todo2.dueDate ? -1 : todo1.dueDate > todo2.dueDate ? 1 : 0;
+            }).map((todo) => (todo.isComplete ? null : <TodoItem key={todo.id} {...todo} completeTodo={completeTodo} />))}
+        </Wrapper>
+    );
 }
 
 const Wrapper = styled.div`
-  border: solid 1px;
-  font-size: 18px;
-  flex: 1;
+    font-size: 18px;
+    flex: 1 1 0%;
 `;

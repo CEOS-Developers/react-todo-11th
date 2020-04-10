d9ff6b3b3357ec6039834b7563809c1876c46a7a
Namil Kim <simyeen@naver.com>
2020-04-10 19:26:38 +0900

    'index.js-1'

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,31 @@ export default function Home() {
   const [counter, setCounter] = useState(1);
 
   const addTodo = ({content, date}) => {
+    if ((content === undefined) || (date === undefined)) {
+      alert('모든 항목을 입력해주세요!');
+      return;
+    }
+
+    else if (String(date).length != 8){
+      alert('날짜를 올바른 형식으로 입력해주세요!');
+      return;
+    }
+
+    const todo = {
+      id: nextId,
+      content,
+      date,
+      checked: false
+    }
+    setTodos(todos.concat(todo)); 
+    nextId.current += 1;
+
+    alert('입력 완료!')
+  }
+  
+  const todoRemove = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
 
   return (
     <Wrapper>
